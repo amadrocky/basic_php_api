a0d90441d8d941c1e37a9c2454c58e56ed1851d5
amadrocky <ak45amad@hotmail.fr>
2021-01-31 18:06:51 +0100

    Config pdo

--- pdo.php
+++ pdo.php
@@ -1,13 +1,13 @@
 <?php
 $host = 'localhost';
 $port = "8000";
-$nom_bdd = 'weather_city';
+$database = 'weather_city';
 $user = ''; //enter your user
 $password =''; //enter your password
 
 try {
 	//On test la connexion à la base de donnée
-    $pdo = new PDO('mysql:host='.$host.';port='.$port.';dbname='.$nom_bdd, $user, $password);
+    $pdo = new PDO('mysql:host='.$host.';port='.$port.';dbname='.$database, $user, $password);
 
 } catch(Exception $e) {
 	//Si la connexion n'est pas établie, on stop le chargement de la page.
